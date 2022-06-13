86f9ada647428fcdd42cb64c58888a92b905147b
Casaemiro <silasmagho@gmail.com>
2022-06-13 15:25:28 +0100

    footer added

--- src/Footer.js
+++ src/Footer.js
@@ -10,6 +10,20 @@ const Footer = () => {
             <a href="#">Locations</a>
             <a href="#">donate</a>
             <a href="#">developers</a>
+            <a href="#">contacts</a>
+            <a href="#">hire</a>
+            <a href="#">scholarship</a>
+            </div>
+            <div className="footer-top-row1">
+          
+            <a href="#">home</a>
+            <a href="#">Jobs</a>
+            <a href="#">Locations</a>
+            <a href="#">donate</a>
+            <a href="#">developers</a>
+            </div>
+            <div>
+                copyright2022
             </div>
         </div>
      );
