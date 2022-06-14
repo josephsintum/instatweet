935fe4cf1d09b14c34db49fe8149cf1bc90de6c5
josephsintum <josephsintum@gmail.com>
2022-06-14 07:17:23 -0700

    add form logic

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,10 +1,20 @@
-import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
-
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-      // Process a POST request
+  if (req.method === "POST") {
+    // Process a POST request
+    if (req.body) {
+      // send to database
+      // check if username is unique
+      // add user to DB
+      // return success message + user session
+      console.log(JSON.parse(req.body))
+      return res.status(200).json({ result: "success" })
     } else {
-      // Handle any other HTTP method
-      return res.status(405).json({error:"This route only supports POST request"})
+      return res.status(500).json({ error: "Form Data is empty" })
     }
+  } else {
+    // Handle any other HTTP method
+    return res
+      .status(405)
+      .json({ error: "This route only supports POST request" })
   }
+}
--- pages/signup.js
+++ pages/signup.js
@@ -1,54 +1,62 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Footer from '../src/Footer';
+import * as React from "react"
+import Avatar from "@mui/material/Avatar"
+import Button from "@mui/material/Button"
+import CssBaseline from "@mui/material/CssBaseline"
+import TextField from "@mui/material/TextField"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import Checkbox from "@mui/material/Checkbox"
+import Link from "@mui/material/Link"
+import Grid from "@mui/material/Grid"
+import Box from "@mui/material/Box"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import Footer from "../src/Footer"
 import { css } from "@emotion/css"
 
-
-const theme = createTheme();
 export default function SignUp() {
   const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    // call the backend function
+
+    const formData = {
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
+    }
+
+    fetch("/api/signup", { method: "POST", body: JSON.stringify(formData) })
+  }
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             border: "1px  solid  #d0e1f0",
             padding: "6%",
-            borderRadius: "10px"
+            borderRadius: "10px",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -94,7 +102,9 @@ export default function SignUp() {
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
@@ -109,7 +119,13 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/login" variant="body2" className={css`text-decoration:none`}>
+                <Link
+                  href="/login"
+                  variant="body2"
+                  className={css`
+                    text-decoration: none;
+                  `}
+                >
                   Already have an account? Sign in
                 </Link>
               </Grid>
@@ -118,6 +134,6 @@ export default function SignUp() {
         </Box>
       </Container>
       <Footer />
-    </ThemeProvider>
-  );
+    </>
+  )
 }
